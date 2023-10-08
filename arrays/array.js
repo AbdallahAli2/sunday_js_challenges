a725dfcac944edd9e7823c3b750d494e822682b5
AbdallahAli2 <119799728+AbdallahAli2@users.noreply.github.com>
2023-10-08 15:50:11 +0100

    8-10-23 - finishing for the day

---
+++ arrays/array.js
@@ -0,0 +1,9 @@
+let friendsAndFamily = ["Abdallah", "Zunairah", "Suleiman", "Nooralain", "Rayaan", "Aalia", "Anwar", "Salman", "Omar", "Cilmi", "Sacdiyo Yareesay"]
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+var random = getRandomInt(11)
+function sayMyName(random) {
+    alert(random)
+
+}
